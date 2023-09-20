8a317d0939e48e783382c6fd08d17ca23006a909
Vulps <ajmcallister5@gmail.com>
2023-09-20 04:31:34 +0100

    just work... please

--- vite.config.js
+++ vite.config.js
@@ -3,27 +3,27 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+	plugins: [
+		laravel({
+			input: [
+				'resources/sass/app.scss',
+				'resources/js/app.js',
 				'resources/css/app.css'
-            ],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-        },
-    },
+			],
+			refresh: true,
+		}),
+		vue({
+			template: {
+				transformAssetUrls: {
+					base: null,
+					includeAbsolute: false,
+				},
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			vue: 'vue/dist/vue.esm-bundler.js',
+		},
+	},
 });
