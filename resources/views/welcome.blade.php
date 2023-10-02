5337a87e97226c0cb357cf637677f7fc65a3b0be
Vulps <ajmcallister5@gmail.com>
2023-10-02 20:02:02 +0100

    Added little message to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div>
+	<h1> Smashr is in pre-alpha </h1>
+	<h2> You may log in and sign up but be aware that the site is not yet fully functional and data protection has <strong>not</strong> been implemented</h2>
+</dv>
 @endsection
