cc28114063d8b12acc90dd42bb2e06b9c39267f5
Vulps <ajmcallister5@gmail.com>
2023-10-15 21:13:03 +0100

    Extracted Photocard function

--- public/js/photomanager.js
+++ public/js/photomanager.js
@@ -25,25 +25,28 @@ function handleImageUpload(event) {
 			if (data.url) {
 				console.log(data)
 				// Assuming 'position' is the position of the photo card
-				const position = data.position;
-				const photoCardContainer = document.getElementById(`photo-card-${position}`);
-				console.log(photoCardContainer);
-				if (photoCardContainer) {
-					console.log("Container found")
-					// Create a new image element and set its attributes
-					const newImage = document.createElement("img");
-					newImage.src = data.url;
-					newImage.alt = "Profile image";
-					newImage.className = "w-full h-40 object-cover rounded-lg";
-					console.log("New Image created")
-					// Replace the existing content with the new image
-					photoCardContainer.innerHTML = '';
-					photoCardContainer.appendChild(newImage);
-					console.log("Update complete")
-				}
+				updatePhotoCard(data.position, data.url);
 			}
 		})
 		.catch(error => {
 			console.error(error);
 		})
+}
+
+function updatePhotoCard(position, url) {
+	const photoCardContainer = document.getElementById(`photo-card-${position}`);
+	console.log(photoCardContainer);
+	if (photoCardContainer) {
+		console.log("Container found")
+		// Create a new image element and set its attributes
+		const newImage = document.createElement("img");
+		newImage.src = data.url;
+		newImage.alt = "Profile image";
+		newImage.className = "w-full h-40 object-cover rounded-lg";
+		console.log("New Image created")
+		// Replace the existing content with the new image
+		photoCardContainer.innerHTML = '';
+		photoCardContainer.appendChild(newImage);
+		console.log("Update complete")
+	}
 }
