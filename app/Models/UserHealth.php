dcdf55bd8c09d2ec65ba344be9d548a991318d19
Vulps <ajmcallister5@gmail.com>
2024-02-24 00:11:39 +0000

    Added Documentation and isMe checks

--- app/Models/UserHealth.php
+++ app/Models/UserHealth.php
@@ -5,6 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Fillable properties for the UserHealth model.
+ *
+ * @property int $user_id The ID of the user associated with the health information.
+ * @property string $hiv_status The HIV status of the user.
+ * @property \Illuminate\Support\Carbon|null $last_STI_test The date of the user's last STI test.
+ * @property bool $on_prep Indicates whether the user is on Pre-exposure Prophylaxis (PrEP).
+ * @property bool $show_hiv_status Indicates whether the user's HIV status should be displayed.
+ */
 class UserHealth extends Model
 {
 	use HasFactory;
@@ -30,6 +39,11 @@ class UserHealth extends Model
 		return $this->belongsTo(User::class);
 	}
 
+	/**
+	 * Get a formatted string representation of the user's HIV status, optionally including Pre-exposure Prophylaxis (PrEP) status.
+	 *
+	 * @return string The formatted string representation of the user's HIV status.
+	 */
 	public function getHIVString()
 	{
 		if (!$this->show_hiv_status) return '';
@@ -39,19 +53,31 @@ class UserHealth extends Model
 		return "{$this->hiv_status} | {$prep}";
 	}
 
+	/**
+	 * Get a formatted string representation of the date of the user's last STI test.
+	 *
+	 * @return string The formatted date of the user's last STI test (dd/mm/yyyy), or an empty string if the date is not to be shown.
+	 */
+
 	public function getLastTestString()
 	{
 		if (!$this->show_last_STI_test) return '';
 		return $this->last_STI_test->format('d/m/Y');
 	}
 
-	public function toArray()
+	/**
+	 * Get an array representation of the UserHealth model instance, optionally formatting certain values based on the context.
+	 *
+	 * @param bool $isMe Optional. If true, returns raw values without additional formatting.
+	 * @return array An array containing the user's health information, including user ID, HIV status, and last STI test date.
+	 */
+	public function toArray($isMe = false)
 	{
 		return [
 			"user_id" => $this->user_id,
 			"show_hiv_status" => $this->show_hiv_status,
-			"hiv_status" => $this->getHIVString(),
-			"last_test" => $this->getLastTestString()
+			"hiv_status" => $isMe ? $this->hiv_status : $this->getHIVString(),
+			"last_test" => $isMe ? $this->last_STI_test : $this->getLastTestString()
 		];
 	}
 }
